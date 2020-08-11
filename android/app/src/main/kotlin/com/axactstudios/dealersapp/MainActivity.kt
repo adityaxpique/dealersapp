ea3b12e139dd00da881491176760c98a87cebb48
aditya <aditya29ks@gmail.com>
2020-08-11 10:14:43 -0400

    basic ui

---
+++ android/app/src/main/kotlin/com/axactstudios/dealersapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.axactstudios.dealersapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:dealersapp/Screens/login.dart';
+import 'package:dealersapp/Screens/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Axact Studios',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
